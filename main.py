94642f81d99adc823acf5623c5ae4f451dba5c0e
charlesduffour <charles.duffour@student-cs.fr>
2025-12-03 21:24:39 +0100

    Add HTTP server

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from flask import Flask
+import threading
 import time
+import os
 
-print("Collector started...")
+app = Flask(__name__)
 
-while True:
-    # Plus tard : collecte Spotify
-    time.sleep(10)
+@app.route("/")
+def home():
+    return "Collector running OK", 200
+
+# Ton collector en thread
+def collector():
+    print("Collector started...")
+    while True:
+        time.sleep(10)
+
+if __name__ == "__main__":
+    # Lancer le collector dans un thread
+    t = threading.Thread(target=collector, daemon=True)
+    t.start()
+
+    # Lancer le serveur Flask que Fly attend
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
